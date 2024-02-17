15ef675261027ea2c2314ad3b92455288fc4a5e6
cZantana <120389014+cZantana@users.noreply.github.com>
2024-02-17 00:02:22 -0500

    Fifth commit (Project template finished)
    Project template finished, Starting contact section

--- Harley.js
+++ Harley.js
@@ -10,14 +10,14 @@ function isInViewport(el) {
 function Aparicion(section,items,styles)
 {
   // console.log(window.scrollY == 0)
-  console.log(items)
-  if (isInViewport(section) < (window.innerHeight*0.70))
+  // console.log(items)
+  if (isInViewport(section) < (window.innerHeight*0.90))
   {
     let delay = 0.3
-      for (i=0; i < items.length;i++){
+      for (let i=0; i < items.length;i++){
         if (window.getComputedStyle(items[i]).getPropertyValue("opacity") < 1)
         {
-            console.log("entro a poner")
+            // console.log("entro a poner")
             items[i].classList.add(styles)
             items[i].style.transitionDelay = String(delay)+"s"
             delay += 0.2
@@ -28,8 +28,8 @@ function Aparicion(section,items,styles)
   }
   else if ((isInViewport(section) >= (window.innerHeight*0.55)) || window.scrollY == 0){
     let delay = 0
-      for (i=0; i < items.length;i++){
-        console.log("entro a quitar")
+      for (let i=0; i < items.length;i++){
+        // console.log("entro a quitar")
             items[i].classList.remove(styles)
             items[i].style.transitionDelay = String(delay)+"s"
             delay += 0.2
@@ -37,12 +37,48 @@ function Aparicion(section,items,styles)
   }
 }
 
+
+
 const portada = document.querySelector(".portada")
 const conteportada = document.getElementsByClassName("conteportada")
+const secondportada = document.querySelector(".secondportada")
+const secondconteportada = document.getElementsByClassName("secondconteportada")
+
+for (i=0; i < secondconteportada.length;i++){
+  secondconteportada[i].addEventListener('transitionend', (event) => {
+    // La transición ha terminado, puedes realizar acciones aquí
+    event.srcElement.style.transitionDelay = "0s"
+  });
+}
 
 Aparicion(portada,conteportada,"portadaS")
 setTimeout(function(){
   portada.classList.add("portadaStimer")
-  for (i=0; i < conteportada.length;i++){
+  for (let i=0; i < conteportada.length;i++){
   conteportada[i].classList.add("conteportadaStimer")}
+  setTimeout(Aparicion(secondportada,secondconteportada,"secondportadaS"),1000)
 },2000)
+
+
+setTimeout(function(){
+  secondportada.classList.add("secondportadaStimer")
+  for (let i=0; i < secondconteportada.length;i++){
+  secondconteportada[i].classList.add("secondconteportadaStimer")}
+},2000)
+
+const bodys = document.querySelectorAll("body");
+bodys[0].classList.remove("bodyQ");
+
+function delay (URL) {
+  setTimeout(function() {bodys[0].classList.add("bodyQ")}, 100)
+  setTimeout(function() { window.scrollTo(0, 0)}, 500)
+  setTimeout( function() { window.location.href = URL;}, 1000);
+
+}
+
+
+var flkty = new Flickity( '.carousel', {
+  // options
+  // cellAlign: 'left',
+  contain: true
+});
